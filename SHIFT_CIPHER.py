2ac7e1caf06024411dbce8e6e0fdc00d0aa375c5
VAANI BANSAL <140154294+dinosaur21@users.noreply.github.com>
2024-06-03 13:33:43 +0530

    Create SHIFT_CIPHER.py

---
+++ SHIFT_CIPHER.py
@@ -0,0 +1,30 @@
+#shift cipher
+
+def encrypt(text, s):
+    result = []
+
+    for char in text:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result.append(chr((ord(char) - shift + s) % 26 + shift))
+        else:
+            result.append(char)
+
+    return ''.join(result)
+
+print("----------------WELCOME TO SHIFT CIPHER--------------")
+
+def shift_algo():
+    text = input("enter string:  ")
+    s = int(input("enter shift value:   "))
+    print("Text  : " + text)
+    print("Shift : " + str(s))
+    print("Cipher: " + encrypt(text,s))
+    print("\n")
+    ip= input("Do you wanna try again? (yes/no) :   ")
+    if ip=="yes":
+        shift_algo()
+    else:
+        print("EXITING....")
+
+shift_algo()
